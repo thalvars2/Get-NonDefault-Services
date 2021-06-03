0ec9925327e533596f5e0bedf29d21f9b77352d3
TH <47740385+thalvars2@users.noreply.github.com>
2021-06-03 09:14:24 +0200

    Update Get-NonDefault-Services.ps1
    added multiple machines

--- Get-NonDefault-Services.ps1
+++ Get-NonDefault-Services.ps1
@@ -1,15 +1,21 @@
 <#
 .SYNOPSIS
   A script to get all non-default services on a Windows machine.
-
 .NOTES
   Version:        1.0
   Author:         Rickard Carlsson (@tzusec)
   Creation Date:  2020-08-29
 #>
 
+$server = "sltvxadc1","sltvxadc2","sltvxadc3","sltvxadc4","sltvxapvs1","sltvxapvs2","sltvxapvs3","sltvxapvs4"
+
+$server | foreach{
+
+
 $NonDefaultServices = @() 
-$Services = Get-wmiobject win32_service | where { $_.PathName -notmatch "policyhost.exe" -and $_.Name -ne "LSM" -and $_.PathName -notmatch "OSE.EXE" -and $_.PathName -notmatch "OSPPSVC.EXE" -and $_.PathName -notmatch "Microsoft Security Client" -and $_.DisplayName -notmatch "NetSetupSvc" -and $_.Caption -notmatch "Windows" -and $_.PathName -notmatch "Windows"  }
+$Services = Get-wmiobject win32_service -ComputerName $_  | where { $_.PathName -notmatch "policyhost.exe" -and $_.Name -ne "LSM" -and $_.PathName -notmatch "OSE.EXE" -and $_.PathName -notmatch "OSPPSVC.EXE" -and $_.PathName -notmatch "Microsoft Security Client" -and $_.DisplayName -notmatch "NetSetupSvc" -and $_.DisplayName -notmatch "VMware" -and $_.DisplayName -notmatch "McAfee" -and $_.DisplayName -notmatch "Microsoft Monitoring" -and $_.Caption -notmatch "Windows" -and $_.PathName -notmatch "Windows"  }
+
+
 
 Foreach ($Service in $Services) {
     $NonDefaultServices+= [pscustomobject]@{
@@ -20,8 +26,13 @@ Foreach ($Service in $Services) {
         ProcessID = $Service.ProcessId                
         ExePath = $Service.PathName
         Description = $Service.Description
+        
     }
 }
 
+Write-Output "Server : $_ " 
 Write-Output "Found $($NonDefaultServices.Count) non-default services."
-Write-Output $NonDefaultServices
+Write-Output $NonDefaultServices |select displayname
+Write-Output "------------------------------------------" 
+
+}
